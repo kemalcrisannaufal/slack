f836ea8a9ca5727d1425d1da28f5c5ae8d235e5b
unknown <kemalcrisannnaufal@gmail.com>
2024-07-30 14:15:38 +0700

    styles: make styles responsive

--- src/modules/testimonial/TestimonialDetail.jsx
+++ src/modules/testimonial/TestimonialDetail.jsx
@@ -3,27 +3,27 @@ import LearnMoreButton from "../../common/components/LearnMoreButton";
 const TestimonialDetail = () => {
   return (
     <div className="w-full pb-10">
-      <div className="w-full flex items-center">
+      <div className="w-full flex flex-col lg:flex-row items-center">
         <div className="w-full lg:w-1/2">
-          <div className="w-full h-96 rounded-r-full overflow-hidden">
-            <video className="" autoPlay loop muted>
+          <div className="w-full lg:h-96 rounded-r-full overflow-hidden">
+            <video className="w-full h-full object-cover" autoPlay loop muted>
               <source src={"/videos/videoTestimoni.mp4"} />
             </video>
           </div>
         </div>
-        <div className="w-full lg:w-1/2 px-20">
+        <div className="w-full lg:w-1/2 mt-20 px-5 lg:px-20">
           <div>
-            <blockquote className="text-neutral-700 text-2xl italic leading-normal">
+            <blockquote className="text-neutral-700 text-xl lg:text-2xl italic leading-normal">
               “Whether you’re a small or large organization, executing anything
               from a discrete modernization program to a digital transformation
               initiative, Slack is an incredibly powerful tool in the hybrid
               world.”
             </blockquote>
             <div className="mt-5">
-              <p className="text-neutral-800 font-bold text-lg">
+              <p className="text-neutral-800 font-bold text-md lg:text-lg">
                 Jennifer Quinlan
               </p>
-              <p>
+              <p className="text-sm lg:text-md" >
                 Managing Partner, IBM iX Americas Leader - Customer and
                 Experience Transformation, IBM
               </p>
@@ -36,8 +36,8 @@ const TestimonialDetail = () => {
           </div>
         </div>
       </div>
-      <div className="flex justify-center mt-20">
-        <p className="text-sm">
+      <div className="flex justify-center mt-10 lg:mt-20 px-5">
+        <p className="text-xs lg:text-sm text-center">
           * Weighted average. Based on 2,707 survey responses from weekly Slack
           users in the U.S., UK, Australia and Canada with a ± 2% margin of
           error at 95% CI (December 2021).
